61425cf55d3d0a5b59533a92c325fa0575b2ad01
Zion Koyl <hiszd1@gmail.com>
2023-08-09 09:27:47 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,8 +2,6 @@
   description = "Example kickstart Nix development setup.";
 
   inputs = {
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
-    darwin.url = "github:lnl7/nix-darwin";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager/release-23.05";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
@@ -16,16 +14,10 @@
       ### END OPTIONS ###
 
       ### START SYSTEMS ###
-      darwin-system = import ./system/darwin.nix { inherit inputs username; };
       nixos-system = import ./system/nixos.nix { inherit inputs username; };
       ### END SYSTEMS ###
     in
     {
-      darwinConfigurations = {
-        darwin-aarch64 = darwin-system "aarch64-darwin";
-        darwin-x86_64 = darwin-system "x86_64-darwin";
-      };
-
       nixosConfigurations = {
         nixos-aarch64 = nixos-system "aarch64-linux";
         nixos-x86_64 = nixos-system "x86_64-linux";
