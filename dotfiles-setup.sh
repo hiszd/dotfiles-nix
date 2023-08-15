8fef4518f0ff5a388a3b9f823eb8238b047242b8
hiszd <hiszd1@gmail.com>
2023-08-15 14:24:19 -0400

    fixing shell script

--- dotfiles-setup.sh
+++ dotfiles-setup.sh
@@ -1,5 +1,4 @@
 nix-shell -p git --run "
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME' &&
 git clone --bare https://github.com/hiszd/.dot-files $HOME/.cfg &&
-config checkout &&
-config config --local status.showUntrackedFiles no"
+git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout &&
+git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no"
