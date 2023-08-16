42065cb8f79d9002bdb0e41ca2281008a11c8bba
Zion Koyl <hiszd1@gmail.com>
2023-08-16 16:24:48 -0400

    fixed dotfiles for nix branch and git

--- dotfiles-setup.sh
+++ dotfiles-setup.sh
@@ -2,6 +2,9 @@ sudo rm -r ~/.config/hypr
 sudo rm -r ~/.config/fish
 
 nix-shell -p git --run "
+git config --global credential.helper store &&
+git config --global user.email "hiszd1@gmail.com" &&
+git config --global user.name "Zion Koyl" &&
 git clone --bare https://github.com/hiszd/.dot-files $HOME/.cfg &&
 git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout nix &&
 git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no &&
