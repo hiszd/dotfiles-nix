b8563ac5676ac6b756238b9f6ce942ceee2f1747
Zion Koyl <hiszd1@gmail.com>
2024-05-20 13:54:28 -0400

    Dev initial commit

--- dotfiles-setup.sh
+++
@@ -1,4 +0,0 @@
-sudo rm -r ~/.config/hypr
-sudo rm -r ~/.config/fish
-
-yadm clone https://github.com/hiszd/dotfiles --bootstrap
--- hardware-setup.sh
+++
@@ -1,3 +0,0 @@
-nix-shell -p git --run "
-sudo git add --intent-to-add system/hardware-configuration.nix
-git update-index --assume-unchanged system/hardware-configuration.nix"
